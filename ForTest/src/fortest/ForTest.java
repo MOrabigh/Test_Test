c677508bbb1b1388451083adef80c2b5d5515641
Abdulaziz <Abdulaziz@DESKTOP-UKNRH8R>
2019-03-25 23:21:37 +0300

    TTT

--- ForTest/src/fortest/ForTest.java
+++ ForTest/src/fortest/ForTest.java
@@ -17,7 +17,10 @@ public class ForTest {
     public static void main(String[] args) {
 
         System.out.println("Abdulaziz");
-
+        
+        
+  System.out.println("ففففففف");
+    System.out.println("يييييي");
     }
     
 }
