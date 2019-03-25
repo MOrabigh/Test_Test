2f12dd23076ba443bfceada2838aed8a90faf730
algha <algha@DESKTOP-LUOB9PJ>
2019-03-25 23:26:31 +0300

    111

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -15,7 +15,7 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAA");
+        System.out.println("AAAAAAAA");
         
         
     }
